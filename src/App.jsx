39e2fc6cbe2e68a786bb8a05aa7b333787caea40
Alena Sharai <exultantislupus@gmail.com>
2025-09-04 15:54:54 +0100

    feat: create Heading component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import Heading from './ui/Heading';
 
 const StyledApp = styled.div`
   padding: 20px;
@@ -17,9 +13,13 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The wild oasis</H1>
+        <Heading as='h1'>The wild oasis</Heading>
+
+        <Heading as='h2'>Check in and out</Heading>
         <Button onClick={() => alert('Check in')}>Check in</Button>
         <Button onClick={() => alert('Check out')}>Check out</Button>
+
+        <Heading as='h3'>Forms</Heading>
         <Input type='number' placeholder='Number of guests'></Input>
         <Input type='number' placeholder='Number of guests'></Input>
       </StyledApp>
