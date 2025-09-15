5bf87c799c5ff87e5e9b8d0e6b6e80172e59b13d
Alena Sharai <exultantislupus@gmail.com>
2025-09-15 20:26:58 +0100

    feat: create update settings functionality

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -4,6 +4,7 @@ import Input from '../../ui/Input';
 import Spinner from '../../ui/Spinner';
 
 import { useSettings } from './useSetting';
+import { useUpdateSetting } from './useUpdateSetting';
 
 function UpdateSettingsForm() {
   const {
@@ -15,29 +16,54 @@ function UpdateSettingsForm() {
     } = {},
     isLoading,
   } = useSettings();
+  const { updateSetting, isUpdating } = useUpdateSetting();
+
+  function handleUpdate(e, fieldName) {
+    const { value } = e.target;
+
+    if (!value) return;
+
+    updateSetting({ [fieldName]: value });
+  }
 
   if (isLoading) return <Spinner />;
 
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input defaultValue={minBookingLength} type='number' id='min-nights' />
+        <Input
+          defaultValue={minBookingLength}
+          disabled={isUpdating}
+          type='number'
+          id='min-nights'
+          onBlur={(e) => handleUpdate(e, 'minBookingLength')}
+        />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input defaultValue={maxBookingLength} type='number' id='max-nights' />
+        <Input
+          defaultValue={maxBookingLength}
+          disabled={isUpdating}
+          type='number'
+          id='max-nights'
+          onBlur={(e) => handleUpdate(e, 'maxBookingLength')}
+        />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
         <Input
           defaultValue={maxGuestsPerBooking}
+          disabled={isUpdating}
           type='number'
           id='max-guests'
+          onBlur={(e) => handleUpdate(e, 'maxGuestsPerBooking')}
         />
       </FormRow>
       <FormRow label='Breakfast price'>
         <Input
           defaultValue={breakfastPrice}
+          disabled={isUpdating}
           type='number'
           id='breakfast-price'
+          onBlur={(e) => handleUpdate(e, 'breakfastPrice')}
         />
       </FormRow>
     </Form>
---
+++ src/features/settings/useUpdateSetting.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updateSetting as updateSettingAPI } from '../../services/apiSettings';
+import toast from 'react-hot-toast';
+
+export function useUpdateSetting() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateSetting, isPending: isUpdating } = useMutation({
+    mutationFn: updateSettingAPI,
+    onSuccess: () => {
+      toast.success('Setting successfully updated!');
+      queryClient.invalidateQueries({ queryKey: ['settings'] });
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return { updateSetting, isUpdating };
+}
