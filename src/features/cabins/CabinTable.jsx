d56757de2d4dd0c87c533177bdf2fb87359a8ea9
Alena Sharai <exultantislupus@gmail.com>
2025-09-17 22:13:23 +0100

    feat: create table body

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -19,9 +19,11 @@ const CabinTable = () => {
         <div>Discount</div>
         <div></div>
       </Table.Header>
-      {cabins.map((cabin) => (
-        <CabinRow cabin={cabin} key={cabin.id} />
-      ))}
+
+      <Table.Body
+        data={cabins}
+        render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+      />
     </Table>
   );
 };
