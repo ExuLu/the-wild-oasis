f1500ab85209ea9477c2f1fd6e269690a4e028b9
Alena Sharai <exultantislupus@gmail.com>
2025-09-22 12:21:06 +0100

    chores: add condition to the page changing

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -38,7 +38,10 @@ const FilterButton = styled.button`
 const Filter = ({ filterField, options }) => {
   const [searchParams, setSearchParams] = useSearchParams();
   const handleClick = (value) => {
-    searchParams.set('page', 1);
+    if (searchParams.get('page')) {
+      searchParams.set('page', 1);
+    }
+
     searchParams.set(filterField, value);
     setSearchParams(searchParams);
   };
